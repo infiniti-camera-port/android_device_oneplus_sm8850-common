082c3eb2163785344abf0a4a44056efbb9c6ac7e
chandu078 <chandudyavanapelli03@gmail.com>
2026-05-31 08:20:13 +0000

    sm8850-common: Switch to infiniti blobs
    Change-Id: If8d3334f6b6dd047be6d25b5412b80dbbff03341

--- extract-files.py
+++ extract-files.py
@@ -65,6 +65,7 @@ blob_fixups: blob_fixups_user_type = {
         'odm/bin/hw/vendor-oplus-hardware-touch-V2-hbp5-service',
         'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff',
         'odm/bin/touchDaemon',
+        'odm/lib64/libdisplayadfr2minfps_qcom.so',
         'odm/lib64/libdisplayfossfeature_nature.so',
         'odm/lib64/libstc_color_feature.so',
         'vendor/bin/hw/audiohalservice.qti',
@@ -145,6 +146,13 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.graphics.composer3-V3-ndk.so', 'android.hardware.graphics.composer3-V4-ndk.so'),
     'vendor/lib64/libsdmcore.so': blob_fixup()
         .add_needed('libbase.so'),
+    (
+        'vendor/bin/wifi_qos_daemon',
+        'vendor/lib64/libVoiceSdk.so',
+        'vendor/lib64/libcapiv2uvvendor.so',
+        'vendor/lib64/liblistensoundmodel2vendor.so',
+    ): blob_fixup()
+        .replace_needed('libtensorflowlite_c.so', 'libtensorflowlite_c_vendor.so'),
     'vendor/usr/keylayout/gpio-keys.kl': blob_fixup()
         .add_line_if_missing('key 735   ASSIST'),
 }  # fmt: skip
