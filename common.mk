412318fc047cab55ffaab9f7a382a0aaebde22c2
1vivy <1vivy@tutanota.com>
2026-07-25 23:55:03 +0000

    sm8850-common: complete Dolby Vision Service sepolicy/props/vintf wiring
    The DVS relocation to sm8850-common shipped the blobs + dvs_aidl.te but not its
    companions. Add file_contexts (binary label), service_contexts (AIDL reg),
    property.te/property_contexts (dolbyv props), the dolby_vision odm props, the
    device_framework_matrix (optional vendor.dolby.dvs HAL), and wire
    BOARD_VENDOR_SEPOLICY_DIRS so the policy actually compiles. Common to all sm8850.
    
    Change-Id: Id3841c72d42a34e01182c8a23bdf8a8a3e0098fd

--- common.mk
+++ common.mk
@@ -458,6 +458,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
+    device/oneplus/sm8850-common/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
     $(LOCAL_PATH)/vintf/manifest_canoe.xml
